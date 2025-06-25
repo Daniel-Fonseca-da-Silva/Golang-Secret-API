e6a34afac09d062aa96943aa8e4b61a405e8a742
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-06-25 19:17:53 +0100

    feat: created routes of user

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Daniel-Fonseca-da-Silva/Golang-Secret-API/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser/:userId", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+}
