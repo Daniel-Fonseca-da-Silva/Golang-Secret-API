2b5532066d12c7f1b33123d61eff3ca2cd6c8912
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-06-26 14:16:09 +0100

    feat: added validator on create user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/Daniel-Fonseca-da-Silva/Golang-Secret-API/src/configuration/rest_err"
+	"github.com/Daniel-Fonseca-da-Silva/Golang-Secret-API/src/configuration/validation"
 	"github.com/Daniel-Fonseca-da-Silva/Golang-Secret-API/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,8 +12,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
